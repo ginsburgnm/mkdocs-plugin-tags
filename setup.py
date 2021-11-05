5b794d47eb46376bfb755ae3932843e057396d12
Ginsburg <n0g@ngin-mbp.ornl.gov>
2021-11-05 14:35:50 -0400

    prep for release

--- setup.py
+++ setup.py
@@ -1,42 +1,14 @@
-# --------------------------------------------
-# Setup file for the package
-#
-# JL Diaz (c) 2019
-# --------------------------------------------
-
-import os
+"""
+Setup the plugin
+"""
 from setuptools import setup, find_packages
-
-
-def read_file(fname):
-    "Read a local file"
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
+import versioneer
 
 setup(
-    name='tags-macros-plugin',
-    version='0.3.0',
-    description="Processes tags in yaml metadata",
-    long_description=read_file('README.md'),
-    long_description_content_type='text/markdown',
-    keywords='mkdocs python markdown tags',
-    url='',
-    author='JL Diaz',
-    author_email='jldiaz@gmail.com',
-    license='MIT',
+    version=versioneer.get_version(),
     python_requires='>=3.6',
     install_requires=[
-        'mkdocs>=1.1',
-        'jinja2',
-    ],
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Information Technology',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.6',
+        'mkdocs==1.2.3',
     ],
     packages=find_packages(exclude=['*.tests']),
     package_data={'tags': ['templates/*.md.template']},
